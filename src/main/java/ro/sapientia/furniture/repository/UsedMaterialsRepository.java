ed5c673232a0528d8ea70c099707d7b045d28609
FranciskaNemeth <franciska99n@gmail.com>
2022-11-29 13:39:45 +0200

    created and edited UsedMaterial controller, repository and service, code refactor: renamed classes

--- src/main/java/ro/sapientia/furniture/repository/UsedMaterialsRepository.java
+++
@@ -1,8 +0,0 @@
-package ro.sapientia.furniture.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ro.sapientia.furniture.model.UsedMaterials;
-
-public interface UsedMaterialsRepository extends JpaRepository<UsedMaterials, Long> {
-    UsedMaterials findUsedMaterialsById(final Long id);
-}
